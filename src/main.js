0fdeddc68d5de1c8936e857da993c4881203c145
Md Rafsan Jani Rafin <105141231+rafsancodeshaper@users.noreply.github.com>
2023-01-16 13:52:04 +0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import App from './App.vue'
 import VueImageLightboxCarousel from './components/VueImageLightboxCarousel'
-import VueLazyload from 'vue-lazyload'
-import loadingImage from './assets/loading.gif'
 
 export default VueImageLightboxCarousel
